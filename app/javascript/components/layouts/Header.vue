b59699e18840c6eac7c5c1551f293fa8e2116931
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-30 14:54:32 +0900

    add Header.vue components & using

---
+++ app/javascript/components/layouts/Header.vue
@@ -0,0 +1,20 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">ドコ食べ</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          メニュー
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Top</a>
+          <a class="dropdown-item" href="#">About</a>
+          <a class="dropdown-item" href="#">Contact</a>
+        </div>
+      </li>
+  </div>
+</nav>
+</template>
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue/dist/vue.esm'
+import Header from '../components/layouts/Header.vue'
 
 const index = new Vue({
     el: "#index",
+    components: {
+        'nav-bar': Header
+    },
     data: function(){
         return {
             message: "Hello World!"
