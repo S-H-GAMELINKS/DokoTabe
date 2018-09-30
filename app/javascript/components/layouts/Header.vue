ecf03efa0aca5f98f611cbeb76e7394ab7b5ca04
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-30 15:02:25 +0900

    replace Header.vue components

--- app/javascript/components/layouts/Header.vue
+++ app/javascript/components/layouts/Header.vue
@@ -1,20 +1,17 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="#">ドコ食べ</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          メニュー
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Top</a>
-          <a class="dropdown-item" href="#">About</a>
-          <a class="dropdown-item" href="#">Contact</a>
-        </div>
-      </li>
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      メニュー
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <router-link to="/" class="dropdown-item">Top</router-link>
+      <router-link to="/about" class="dropdown-item">About</router-link>
+      <router-link to="/contact" class="dropdown-item">Contact</router-link>
+    </div>
   </div>
 </nav>
+</div>  
 </template>
