c1ec74fbdc3681772050034c82d4e85167a0eff1
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-30 15:10:16 +0900

    using SPA move for vue-router

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue/dist/vue.esm'
 import Header from '../components/layouts/Header.vue'
+import Router from '../router/router.js'
 
 const index = new Vue({
+    router: Router,
     el: "#index",
     components: {
         'nav-bar': Header
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'web#index'
+  get '/about',   to: 'accounts#index'
+  get '/contact',   to: 'accounts#index'
   get 'web/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
