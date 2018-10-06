6643bfcb687ecab1fa4682a0dd31f6c1ef252c73
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-06 13:34:53 +0900

    add init Google Map

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue/dist/vue.esm'
+import * as VueGoogleMaps from 'vue2-google-maps'
 import Header from '../components/layouts/Header.vue'
 import Router from '../router/router.js'
+
+Vue.use(VueGoogleMaps, {
+    load: {
+      key: String(gon.google_map_key),
+      libraries: 'places', 
+    },
+})
 
 const index = new Vue({
     router: Router,
