156475b18a66ba46a6b4ddd5f114a5859ce02def
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-08 23:04:47 +0900

    add contact

--- app/javascript/components/web/Contact.vue
+++ app/javascript/components/web/Contact.vue
@@ -1,5 +1,7 @@
 <template>
 <div>
     <h1>Contact Pages</h1>
+    <p><a href="https://github.com/S-H-GAMELINKS">GitHub</a></p>
+    <p><a href="https://gamelinks007.net">Mastodon</a></p>
 </div>
 </template>
