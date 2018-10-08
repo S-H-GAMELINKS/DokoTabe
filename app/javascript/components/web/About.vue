d6d222286f923f54038378f90cdb1074fd32e58c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-08 23:02:28 +0900

    add credit

--- app/javascript/components/web/About.vue
+++ app/javascript/components/web/About.vue
@@ -4,5 +4,8 @@
     <p>現在位置の情報から周辺のお店の情報を取得できるWebサービスです。</p>
     <p>使い方は非常に簡単で、「周辺のお店を検索」というボタンを押すだけ</p>
     <p>お店の情報に関しては、「ぐるなび」のAPIを使用しています。</p>
+    <a href="https://api.gnavi.co.jp/api/scope/" target="_blank">
+<img src="https://api.gnavi.co.jp/api/img/credit/api_265_65.gif" width="265" height="65" border="0" alt="グルメ情報検索サイト　ぐるなび">
+</a>
 </div>
 </template>
